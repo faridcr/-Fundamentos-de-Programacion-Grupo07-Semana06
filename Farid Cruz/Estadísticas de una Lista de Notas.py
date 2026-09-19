78b69e35b1b1890b926f78ca6bc9bdef2812f5ed
Farid Cruz <vilcasoto5@gmail.com>
2026-09-18 20:03:07 -0500

    Operaciones CRUD en Agenda de Contactos

--- Farid Cruz/Estadísticas de una Lista de Notas.py
+++ Farid Cruz/Estadísticas de una Lista de Notas.py
@@ -5,19 +5,18 @@ def calcular_estadisticas(notas):
     promedio = sum(notas) / len(notas)
     nota_alta = max(notas)
     nota_baja = min(notas)
-    aprobados = 0
-    for nota in notas:
-        if nota >= 11:
-         aprobados += 1
-    desaprobados = 0
+    # Generador expresion (una linea mas compacta)
+    aprobados = sum(1 for nota in notas if nota >= 11)
+    # Con una bucle for (variante con mas lineas, pero se ve paso a paso lo que pasa)
+    desaprobados = 0      # desaprobados = sum(1 for nota in notas if nota < 11)
     for nota in notas:
         if nota < 11:
-         desaprobados += 1
+            desaprobados += 1
 
     return promedio, nota_alta, nota_baja, aprobados, desaprobados
 
 notas_alumnos = [15, 18, 12, 9, 17, 14, 20, 11, 16, 13]
-promedio, nota_alta, nota_baja, aprobados = calcular_estadisticas(notas_alumnos)
+promedio, nota_alta, nota_baja, aprobados, desaprobados = calcular_estadisticas(notas_alumnos)
 
 print("\n========== Estadísticas de Notas ==========")
 print(f"Notas: {notas_alumnos}")
